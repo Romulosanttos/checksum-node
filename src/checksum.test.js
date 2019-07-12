a66d705dac2cb52fe0f445bab40ebfacb6da62cd
romulosanttos <developerezydoo@gmail.com>
2019-07-12 11:02:36 -0300

    organizando arquivos usado no teste

--- src/checksum.test.js
+++ src/checksum.test.js
@@ -6,14 +6,14 @@ import checksum from './checksum';
 
 describe('unit: checksum', () => {
 	it('runs with a jpg', async () => {
-		const stream = fs.createReadStream(path.resolve(__dirname, '../test/photo1.jpg'));
+		const stream = fs.createReadStream(path.resolve(__dirname, '../assets/photo1.jpg'));
 		const res = await checksum(stream);
 		expect(res.sum).to.equal('b53397f48ead8563b86a817449c5ada8ce3db276');
 		expect(res.size).to.equal(85699);
 	});
 
 	it('runs with md5 algorithm', async () => {
-		const stream = fs.createReadStream(path.resolve(__dirname, '../test/photo1.jpg'));
+		const stream = fs.createReadStream(path.resolve(__dirname, '../assets/photo1.jpg'));
 		const res = await checksum(stream, { algorithm: 'md5' });
 		expect(res.sum).to.equal('7e0d6e76cfb001f73d241fd9ada11573');
 	});
--- src/snapshot.test.js
+++ src/snapshot.test.js
@@ -5,20 +5,20 @@ import snapshot from './snapshot';
 describe('unit: snapshot', () => {
 	it('runs with a jpg', async () => {
 		const files = [
-			'test/photo1.jpg',
-			'test/photo2.jpg'
+			'assets/photo1.jpg',
+			'assets/photo2.jpg'
 		];
 		expect(await snapshot(files)).to.deep.equal({
 			sum: '0fb3afe0fb94399a6c4a863d19534c77c64c5c3d',
 			size: 203735,
 			snapshot: [
 				{
-					name: 'test/photo1.jpg',
+					name: 'assets/photo1.jpg',
 					sum: 'b53397f48ead8563b86a817449c5ada8ce3db276',
 					size: 85699
 				},
 				{
-					name: 'test/photo2.jpg',
+					name: 'assets/photo2.jpg',
 					sum: 'b0c476c36ee7f9f462713de6e1a97adeb78cfe25',
 					size: 118036
 				}
@@ -28,20 +28,20 @@ describe('unit: snapshot', () => {
 
 	it('runs with md5 algorithm', async () => {
 		const files = [
-			'test/photo1.jpg',
-			'test/photo2.jpg'
+			'assets/photo1.jpg',
+			'assets/photo2.jpg'
 		];
 		expect(await snapshot(files, { algorithm: 'md5' })).to.deep.equal({
 			sum: 'c006c94c4ba83aeb68f3a2b06dd147d679738830',
 			size: 203735,
 			snapshot: [
 				{
-					name: 'test/photo1.jpg',
+					name: 'assets/photo1.jpg',
 					sum: '7e0d6e76cfb001f73d241fd9ada11573',
 					size: 85699
 				},
 				{
-					name: 'test/photo2.jpg',
+					name: 'assets/photo2.jpg',
 					sum: '5d860820a67246913a3fa400cbd104ba',
 					size: 118036
 				}
